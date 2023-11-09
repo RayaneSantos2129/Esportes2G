ab927295e0a1170c6e70a2c9f9410a91dc35817e
Rayane Vieira <rayvieiraaa@gmail.com>
2023-11-09 07:52:43 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Kitty!");
+
+        Pessoa qualquer = new Pessoa();
+        qualquer.setNome("Rebeca Andrade");
+        System.out.println(qualquer.getNome());
+
+    }
+}
