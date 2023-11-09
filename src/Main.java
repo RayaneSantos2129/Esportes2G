86f08faef24673178fdb98ec93f09a515ab2f802
Rayane Vieira <rayvieiraaa@gmail.com>
2023-11-09 08:24:29 -0300

    polimorfismo no volei

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,19 @@ public class Main {
         qualquer.setNome("Rebeca Andrade");
         System.out.println(qualquer.getNome());
 
+        Esportista jogadora = new Esportista();
+        Esportista surfista = new Esportista(345);
+        jogadora.setNome("Thaísa do Volei");
+        System.out.println(jogadora.getNome());
+
+        // Polimorfismo
+        Pessoa outra = new Pessoa();
+        String retorno = outra.sacar();
+        System.out.println(retorno);
+        outra = new Esportista();
+        retorno = outra.sacar();
+        System.out.println(retorno);
+
+
     }
 }
